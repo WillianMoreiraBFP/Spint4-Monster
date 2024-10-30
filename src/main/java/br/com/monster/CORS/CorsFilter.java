32121e707c6db231f9b2004c82b06a85a01c4745
Arthur Arraes <109257634+arthurarraes@users.noreply.github.com>
2024-10-30 11:26:34 -0300

    .

---
+++ src/main/java/br/com/monster/CORS/CorsFilter.java
@@ -0,0 +1,24 @@
+package br.com.monster.CORS;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+
+        // For OPTIONS requests, set a preflight response
+        if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
+            responseContext.getHeaders().add("Access-Control-Max-Age", "86400"); // Cache for 1 day
+            responseContext.setStatus(200);
+        }
+    }
+}
--- src/main/java/br/com/monster/Service/VeiculoService.java
+++ src/main/java/br/com/monster/Service/VeiculoService.java
@@ -1,6 +1,7 @@
 package br.com.monster.Service;
 
 import br.com.monster.DAO.DAO;
+import br.com.monster.model.user.Login;
 import br.com.monster.model.veiculo.Veiculo;
 
 import java.sql.SQLException;
@@ -9,17 +10,19 @@ import java.util.List;
 public class VeiculoService {
     DAO d = new DAO();
 
-    public List<Veiculo> listarVeiculo (int id) throws SQLException{
+    public List<Veiculo> listarVeiculo (String email) throws SQLException{
         List<Veiculo> v;
+        Login login = d.getDados (email);
 
-        v = d.getListaVeiculo(id);
+        v = d.getListaVeiculo(login.getId ());
 
         return v;
     }
 
     public void cadastarVeiculo(Veiculo veiculo) throws SQLException{
 
-        d.cadastroVeiculo(veiculo);
+        Login login = d.getDados (veiculo.getEmail ());
+        d.cadastroVeiculo(veiculo, login.getId ());
 
     }
 
